8caaaac93622c2a5ca36fdc692269506124c1979
bukhudai <bulat.khudainazarov@digia.com>
2023-01-24 15:26:35 +0100

    weihnachtsbaum

---
+++ main.py
@@ -0,0 +1,9 @@
+import turtle
+
+import weihnachtsbaum
+
+turtle.left(90)
+
+weihnachtsbaum.drawTree(4, 40)
+
+turtle.mainloop()
---
+++ weihnachtsbaum/__init__.py
@@ -0,0 +1,20 @@
+import turtle
+
+angle = 117
+
+def drawTree(h, size):
+    if h < 0:
+        return
+    length = size * 1.35 ** h
+    turtle.forward(length)
+    drawTree(h - 1, size)
+    turtle.right(angle)
+    drawTree(h - 2, size)
+    turtle.right(2 * -angle)
+    drawTree(h - 2, size)
+    turtle.right(angle)
+    turtle.forward(-length)
+# Example usage
+# turtle.penup()
+# turtle.goto(0, -200)
+# turtle.pendown()
